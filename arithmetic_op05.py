1d26b79aa6ceaa72790578bc691bb41b40fbb90f
AktamNosirov <94_aktam@mail.ru>
2022-03-11 19:47:06 +0500

    homework5

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,10 +1,9 @@
 # Create a variable "a"  and assign it the value of the int 72.
-
-# Create a variable "b"  and assign it the value of the int 9.
-
-# Create a variable called "answer".
-
-# Find the division of a and b then assign to "answer".
 a=72
+# Create a variable "b"  and assign it the value of the int 9.
 b=9
+# Create a variable called "answer".
 answer=a/b
+# Find the division of a and b then assign to "answer".
+
+
